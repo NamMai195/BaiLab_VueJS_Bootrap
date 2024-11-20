ad554bf93e8e58991ad3983191cb72bb5d4643a2
NamMai95 <mnam3239@gmail.com>
2024-11-21 00:48:14 +0700

    update lab 6

---
+++ lab6/src/components/bai1.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="card">
+     <h2>Nhập điểm đi cu</h2>
+    <div class="card-body">
+        <input v-model="index" class="card-title" type="text"></input>
+        <p class="card-text" v-if='index < 5 && index>=0'>Yếu</p>
+        <p class="card-text" v-else-if='index >= 5 && index <6.5'>Trung Bình</p>
+        <p class="card-text" v-else-if='index >= 6.5  && index <8'>Khá</p>
+        <p class="card-text" v-else-if='index >= 8  && index <9'>Giỏi</p>
+        <p class="card-text" v-else-if='index >= 9 && index <=10'>Xuất Sắc</p>
+        <p class="card-text" v-else>Điểm không hợp lệ</p>
+    </div>
+   </div>
+   
+    
+</template>
+<script>
+   export default {
+     data() {
+       return {
+         index: 0
+       }
+     }
+   }
+</script>
--- lab6/src/router/index.js
+++ lab6/src/router/index.js
@@ -1,22 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+// import HomeView from '../views/HomeView.vue'
+import Bai1 from '@/components/bai1.vue'
+import Bai2 from '@/components/bai2.vue'
+import Bai3 from '@/components/bai3.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'bai1',
+      component: Bai1,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/bai2',
+      name: 'bai2',
+      component: Bai2,
     },
+    {
+      path: '/bai3',
+      name: 'bai3',
+      component: Bai3,
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    
+    //   component: () => import(''),
+    // },
   ],
 })
 
